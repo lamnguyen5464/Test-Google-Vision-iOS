47d41c8aa220e11b5897ec02bb898487119ec2b1
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-08-01 17:26:27 +0700

    add MLKit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+use_frameworks!
+
+target 'WhereMyImage' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  pod 'GoogleMLKit/ImageLabeling'
+
+  # Pods for WhereMyImage
+
+end
--- WhereMyImage/ViewController.swift
+++ WhereMyImage/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import MLKit
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var img: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let image = UIImage(named: "cat1")
+        
+        img.image = image
+        
+        
+        //
+        
+        let options = ImageLabelerOptions()
+        options.confidenceThreshold = 0.7
+        
+        let labeler = ImageLabeler.imageLabeler(options: options)
+        
+        labeler.process(VisionImage(image: image!)){ labels, error in
+            guard error == nil, let labels = labels else {
+                return
+            }
+            for label in labels{
+                print("label: \(label.index): \(label.confidence) - \(label.text)")
+            }
+            
+        }
+        
     }
-
-
 }
 
